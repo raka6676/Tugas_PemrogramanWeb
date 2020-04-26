357dc9316c9172630fc59853c43e696ea458b0b5
Rzqaf14 <rizqiafif6676@gmail.com>
2020-04-26 23:32:09 +0700

    Update login.php

--- application/views/login.php
+++ application/views/login.php
@@ -73,7 +73,7 @@
           </div>
         </form>
 
-        <div class="login100-more" style="background-image: url('assets/images/bg-01.jpg');">
+        <div class="login100-more" style="background-image: url('assets/images/bg-01.png');">
         </div>
       </div>
     </div>
@@ -84,19 +84,19 @@
   
   
 <!--===============================================================================================-->
-  <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+  <script src="assets/vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-  <script src="vendor/animsition/js/animsition.min.js"></script>
+  <script src="assets/vendor/animsition/js/animsition.min.js"></script>
 <!--===============================================================================================-->
-  <script src="vendor/bootstrap/js/popper.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/popper.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-  <script src="vendor/select2/select2.min.js"></script>
+  <script src="assets/vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-  <script src="vendor/daterangepicker/moment.min.js"></script>
-  <script src="vendor/daterangepicker/daterangepicker.js"></script>
+  <script src="assets/vendor/daterangepicker/moment.min.js"></script>
+  <script src="assets/vendor/daterangepicker/daterangepicker.js"></script>
 <!--===============================================================================================-->
-  <script src="vendor/countdowntime/countdowntime.js"></script>
+  <script src="assets/vendor/countdowntime/countdowntime.js"></script>
 <!--===============================================================================================-->
   <script src="js/main.js"></script>
 
