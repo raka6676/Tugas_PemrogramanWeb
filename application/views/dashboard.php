b7c9d07738a921d613042e3a49502427ca01996b
abdillahfauzan <abdillahfauzani00@gmail.com>
2020-04-26 17:19:06 +0700

    Update dashboard.php

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Blank Page</title>
+  <title>Kantor POS | Blank Page</title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="<?=base_url()?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="<?=base_url()?>assets/bower_components/font-awesome/css/font-awesome.min.css">
