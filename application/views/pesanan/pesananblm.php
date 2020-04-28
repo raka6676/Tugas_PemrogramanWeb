e788c3776edaa11e4f3c9d34d265810a04b889ac
Rzqaf14 <rizqiafif6676@gmail.com>
2020-04-29 04:11:10 +0700

    update pesanan belum proses

---
+++ application/views/pesanan/pesananblm.php
@@ -0,0 +1,57 @@
+<section class="content-header">
+      <h1>
+        Dashboard
+        <small>Data Order</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="https://antaran.posindonesia.co.id/cod/index.php/dashboard/member"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li>Pesanan Belum Diproses</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Data Pesanan Belum diproses</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">                
+                <table id="example1" class="table table-bordered table-striped" >
+                <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Tgl Transaksi</th>
+                  <th>Nama Pemesan</th>
+                  <th>Alamat Pemesan</th>
+                  <th>No Hp Pemesan</th>
+                  <th>Barang Pesanan</th>
+                  <th>Jml Item</th>
+                  <th>Harga Barang</th>
+                  <th>Total Biaya</th>
+                  <th><center>Action</center></th>
+                </tr>
+                </thead>
+                <tbody>
+                                </tbody>
+                <tfoot>
+                <tr>
+                  <th colspan="7" style="text-align: left"> Perhatian : <br>- Data yang tercantum adalah data yg belum diproses/batal, silahkan di hapus (klik icon tong sampah)</th>
+                  <!--<th style="text-align: center;">0</th>
+                  <th style="text-align: right;">0</th>
+                  <th style="text-align: right;">0</th>
+                  <th style="text-align: right;"></th>-->
+                </tr>
+                </tfoot>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col -->
+      </div>
+    </section>
