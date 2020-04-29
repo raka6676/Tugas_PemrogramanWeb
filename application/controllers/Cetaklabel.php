8816b7ff73c30d7f9a843746badfb1cd2082d4f2
Rzqaf14 <rizqiafif6676@gmail.com>
2020-04-29 10:08:35 +0700

    Update Cetaklabel.php

--- application/controllers/Cetaklabel.php
+++ application/controllers/Cetaklabel.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Ceralklabel extends CI_Controller {
+class Cetaklabel extends CI_Controller {
 
 	public function index()
 	{
