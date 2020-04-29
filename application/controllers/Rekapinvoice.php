dc96f8c6130247a192b3baafc40fb90532a65f73
Rzqaf14 <rizqiafif6676@gmail.com>
2020-04-29 10:06:07 +0700

    Update Rekapinvoice.php

---
+++ application/controllers/Rekapinvoice.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Rekapinvoice extends CI_Controller {
+
+	public function index()
+	{
+		check_not_login();
+		$this->template->load('template','laporan/rekapinvoice');
+	}
+}
